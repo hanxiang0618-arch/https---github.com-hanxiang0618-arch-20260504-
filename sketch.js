86c686bf4375ca308d6888683b6cb6cc73f6a230
hanxiang <hanxiang0618@gmail.com>
2026-05-04 09:46:29 +0800

    2.0

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(144, 238, 144); // 淺綠色
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
