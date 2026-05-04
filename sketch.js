287d370b4debc2061407e34dcbd8922b517311fb
hanxiang <hanxiang0618@gmail.com>
2026-05-04 10:02:41 +0800

    3.0

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+let capture;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.hide(); // 隱藏預設產生的 HTML 影片元件
 }
 
 function draw() {
-  background(144, 238, 144); // 淺綠色
+  background('#e7c6ff');
+
+  let w = width * 0.5;
+  let h = height * 0.5;
+  let x = (width - w) / 2;
+  let y = (height - h) / 2;
+
+  push();
+  // 將原點移至影像顯示區域的右側，並水平翻轉達成「左右顛倒」的效果
+  translate(x + w, y);
+  scale(-1, 1);
+  image(capture, 0, 0, w, h);
+  pop();
 }
 
 function windowResized() {
